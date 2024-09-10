29473b21ec9a59a7b1bafe7f7e5b1f1147df160f
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-09-10 22:31:30 +0530

    first commit

---
+++ src/raviranjan/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+def home_page_view(request, *args, **kwargs):
+   qs = PageVisit.objects.all()
+   page_qs = PageVisit.objects.filter(path=request.path)
+   my_title = 'My Page'
+   my_context = {
+      'page_title': my_title,
+      'page_visits_count': page_qs.count(),
+      'total_visits_count': qs.count()
+      }
+   html_template = 'home.html'
+   PageVisit.objects.create(path=request.path)
+
+   return render(request, html_template, my_context)
